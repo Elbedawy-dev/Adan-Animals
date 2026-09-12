361c4e307ab350961f128c0978cefcc9c3f7eea0
mo-elbedewy <moaazelbedawy@gmail.com>
2026-09-12 13:40:44 +0300

    fix: conditionally load TelescopeServiceProvider

--- Backend/bootstrap/providers.php
+++ Backend/bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
-return [
+return array_filter([
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
-];
+    class_exists(\Laravel\Telescope\TelescopeApplicationServiceProvider::class)
+        ? App\Providers\TelescopeServiceProvider::class
+        : null,
+]);
